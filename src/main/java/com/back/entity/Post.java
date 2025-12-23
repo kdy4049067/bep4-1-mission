3645f647c95c72de1a27f7f90a3d3f401fe9bf3f
daeyeon <kdy4410121@naver.com>
2025-12-23 12:26:10 +0900

    0002

---
+++ src/main/java/com/back/entity/Post.java
@@ -0,0 +1,26 @@
+package com.back.entity;
+
+import com.back.jpa.entity.BaseIdAndTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Post extends BaseIdAndTime {
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member author;
+    private String title;
+    @Column(columnDefinition = "LONGTEXT")
+    private String content;
+
+    public Post(Member author, String title, String content) {
+        this.author = author;
+        this.title = title;
+        this.content = content;
+    }
+}
