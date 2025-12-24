96dff5c34f58588a3003c3b60691fff6bb22d08a
daeyeon <kdy4410121@naver.com>
2025-12-24 10:05:22 +0900

    0012

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PostFacade {
+
     private final PostRepository postRepository;
     private final PostWriteUseCase postWriteUseCase;
 
