f4a990ff644c97d5d499ca3be5e9a53893525625
daeyeon <kdy4410121@naver.com>
2025-12-24 10:11:27 +0900

    0013

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -1,9 +1,11 @@
 package com.back.boundedContext.member.app;
 
 import com.back.boundedContext.member.domain.Member;
+import com.back.boundedContext.member.domain.MemberPolicy;
 import com.back.boundedContext.member.out.MemberRepository;
 import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.json.JsonWriter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +16,7 @@ import java.util.Optional;
 public class MemberFacade {
     private final MemberRepository memberRepository;
     private final MemberJoinUseCase memberJoinUserCase;
+    private final MemberPolicy memberPolicy;
 
     @Transactional(readOnly = true)
     public long count() {
@@ -34,5 +37,10 @@ public class MemberFacade {
     @Transactional(readOnly = true)
     public Optional<Member> findById(int id) {
         return memberRepository.findById(id);
+    }
+
+    public String getRandomSecureTip(){
+        return "비밀번호의 유효기간은 %d일 입니다."
+                .formatted(memberPolicy.getNeedToChangePasswordDays());
     }
 }
